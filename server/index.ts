f50c4041f49eb3b690653921e4bc624b83b4c3ba
yinyingnah-oss <yinyingnah@gmail.com>
2026-08-16 15:55:24 +0700

    Fix PORT

--- server/index.ts
+++ server/index.ts
@@ -46,7 +46,7 @@ app.post('/api/webhook/toyyibpay', async (req, res) => {
   }
 });
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`tRPC backend running on http://localhost:${PORT}`);
 });
